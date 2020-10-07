9008c12759bf136f624bde49eaf80208f4894e6f
Simone Gramellini <SIMONE.GRAMELLINI@local.ittcesena.it>
2020-10-07 11:13:47 +0200

    First Step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vett = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(vett[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
