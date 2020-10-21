f2e36eb6f2c8f1539b77915d5108c70f9b827549
MattiaFilippi <mattia.filippi.stud@ispascalcomandi.it>
2020-10-21 11:21:05 +0200

    Ho inserito massimo

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -13,11 +13,15 @@ namespace SortVectors
             int[] vett = new int[10] { 0, 12, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             Ordina(vett);
-
+            int massimo = Massimo(vett);
+           
+            Massimo(vett);
+            
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(vett[i]);
             }
+                Console.WriteLine("Il numero massimo è " + massimo);
             Console.ReadKey();
         }
 
@@ -25,5 +29,10 @@ namespace SortVectors
         {
             Array.Sort(v);
         }
+        public static int Massimo(int[] v)
+        {
+            return v[v.Length - 1];
+        }
     }
+
 }
